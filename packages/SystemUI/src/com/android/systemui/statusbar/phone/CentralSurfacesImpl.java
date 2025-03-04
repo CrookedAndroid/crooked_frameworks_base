069afa738bf68fb5480ac82fb49477d0edb81028
slogar25 <michaelslogar@gmail.com>
2025-03-04 23:13:20 +0000

    SystemUI: add back tunerservice to centralsurfacesimpl.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -231,6 +231,7 @@ import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
 import com.android.systemui.statusbar.window.StatusBarWindowController;
 import com.android.systemui.statusbar.window.StatusBarWindowStateController;
 import com.android.systemui.surfaceeffects.ripple.RippleShader.RippleShape;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.DumpUtilsKt;
 import com.android.systemui.util.WallpaperController;
 import com.android.systemui.util.concurrency.DelayableExecutor;
@@ -468,6 +469,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
     private final UserTracker mUserTracker;
     private final Provider<FingerprintManager> mFingerprintManager;
     private final ActivityStarter mActivityStarter;
+	private final TunerService mTunerService;
 
     private final DisplayMetrics mDisplayMetrics;
 
@@ -717,6 +719,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
             UserTracker userTracker,
             Provider<FingerprintManager> fingerprintManager,
             ActivityStarter activityStarter,
+			TunerService tunerService,
             BrightnessMirrorShowingInteractor brightnessMirrorShowingInteractor,
             GlanceableHubContainerController glanceableHubContainerController,
             EmergencyGestureIntentFactory emergencyGestureIntentFactory,
@@ -813,6 +816,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
         mUserTracker = userTracker;
         mFingerprintManager = fingerprintManager;
         mActivityStarter = activityStarter;
+		mTunerService = tunerService;
         mBrightnessMirrorShowingInteractor = brightnessMirrorShowingInteractor;
         mGlanceableHubContainerController = glanceableHubContainerController;
         mEmergencyGestureIntentFactory = emergencyGestureIntentFactory;
